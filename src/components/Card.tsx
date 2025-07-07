f3aeb60767482cf582e5c49150c9a7286adf047d
Mohammed Ayoub Nedjima <85318131+Mohammed-Nedjima@users.noreply.github.com>
2025-07-07 15:08:08 +0100

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-[var(--color-neutral-white)] sm:rounded-3xl flex sm:flex-row flex-col w-[38.5rem] shadow-md">
+    <div className="bg-[var(--color-neutral-white)] sm:rounded-3xl flex sm:flex-row flex-col w-screen sm:w-[38.5rem] shadow-md">
       {children}
     </div>
   );
